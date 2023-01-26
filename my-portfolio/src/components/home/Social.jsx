15d71a6c8099430c35969a05e5d0489feb23f878
Khiro Sasha <sasha.khiro@gmail.com>
2023-01-26 14:41:26 +0200

    aded new components for view home of website

---
+++ my-portfolio/src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Social = () => {
+    return(
+        <div className="home__social">
+            <a href="https://www.instagram.com/sashawawawa/" className="home__social-icon" target="_blank">
+                <i className="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://t.me/khirooooooo" className="home__social-icon" target="_blank">
+                <i className="uil uil-telegram"></i>
+            </a>
+
+            <a href="https://github.com/KhirovSasha" className="home__social-icon" target="_blank">
+                <i className="uil uil-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
